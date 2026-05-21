3f7d6d42b5050f535c0097bf7d281646c8ab401d
Prodipta <nayebprodipta@gmail.com>
2026-05-21 19:05:56 +0530

    experimenting some changes

--- script.js
+++ script.js
@@ -253,13 +253,13 @@ if (toastTrigger) {
 
 //Google
 
-// window.onload = function () {
+//window.onload = function () {
         
-//         // Initialize the Google client
-//         google.accounts.id.initialize({
-//             client_id: "551517105916-ab64kirluov2pll51dve7qmv5la1dqcc.apps.googleusercontent.com",
-//             callback: handleCredentialResponse
-//         });
+        // Initialize the Google client
+        google.accounts.id.initialize({
+            client_id: "551517105916-ab64kirluov2pll51dve7qmv5la1dqcc.apps.googleusercontent.com",
+            callback: handleCredentialResponse
+        });
 
         // Link your custom button to the Google Sign-In prompt
         const googleButton = document.getElementById('google');
